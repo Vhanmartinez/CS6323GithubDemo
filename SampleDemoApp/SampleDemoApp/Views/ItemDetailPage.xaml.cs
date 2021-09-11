7592c955a8c8fcbcec67fdfce881f970872415c9
neils-realtair <58451929+neil-realtair@users.noreply.github.com>
2021-09-11 14:09:42 +0800

    Initial commit for blank mobile app project

---
+++ SampleDemoApp/SampleDemoApp/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using Xamarin.Forms;
+using SampleDemoApp.ViewModels;
+
+namespace SampleDemoApp.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
---
+++ SampleDemoApp/SampleDemoApp/Views/NewItemPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+using SampleDemoApp.Models;
+using SampleDemoApp.ViewModels;
+
+namespace SampleDemoApp.Views
+{
+    public partial class NewItemPage : ContentPage
+    {
+        public Item Item { get; set; }
+
+        public NewItemPage()
+        {
+            InitializeComponent();
+            BindingContext = new NewItemViewModel();
+        }
+    }
+}
